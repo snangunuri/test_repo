9dea937e5eb3e7045db0a0a5c226e7341ef9fb38
Srinivasa Nangunuri <snangunuri@hotmail.com>
2019-03-28 16:45:11 -0400

    Made changes

--- docker_infra_access_report.sh
+++ docker_infra_access_report.sh
@@ -6,6 +6,6 @@ user_id=`whoami`
 report_file=docker_infra_access_report.csv
 echo "$timestamp $host $user_id" | awk '{ print $1","$2","$3 }' >> $report_file
 
-/usr/bin/git add docker_infra_access_report.csv
+/usr/bin/git add $report_file
 /usr/bin/git commit -m "Adding an entry to the file: $report_file"
 /usr/bin/git push https://github.com/snangunuri/test_repo.git master
