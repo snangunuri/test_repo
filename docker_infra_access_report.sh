e53d25d80e6cb329f0e87302f3baa9b1af05fa15
Srinivasa Nangunuri <snangunuri@hotmail.com>
2019-03-28 16:42:25 -0400

    Changes made

--- docker_infra_access_report.sh
+++ docker_infra_access_report.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 host=`hostname`
-timestamp=`date`
+timestamp=`date "+%b-%d-%Y"`
 user_id=`whoami`
 
-echo "$timestamp;$host;$user_id" | awk -F ";" '{ printf "%-35s %-20s %s\n", $1, $2, $3 }' >> docker_infra_access_report.txt
+report_file=docker_infra_access_report.csv
+echo "$timestamp $host $user_id" | awk '{ print $1","$2","$3 }' >> $report_file
 
-/usr/bin/git add docker_infra_access_report.txt
-/usr/bin/git commit -m "Adding an entry to the file: docker_infra_access_report.txt"
+/usr/bin/git add docker_infra_access_report.csv
+/usr/bin/git commit -m "Adding an entry to the file: $report_file"
 /usr/bin/git push https://github.com/snangunuri/test_repo.git master
